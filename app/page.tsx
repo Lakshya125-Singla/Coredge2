80fc30b7f9d073e688c9b48b75011a901478c08f
LAKSHYA SINGLA <lakshyasingla125@gmail.com>
2026-01-07 15:51:31 +0530

    Footer added in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from './Components/Navbar';
 import Claudai from './Components/Claudai';
 import Theworld from './Components/Theworld'; 
+import Footer from './Components/Footer';
 export default function Home() {
   return (
    <>
     <Navbar />
     <Claudai />
     <Theworld/>
+     <Footer/>
    
    </>
   );
