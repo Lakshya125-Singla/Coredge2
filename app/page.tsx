16a331b10bf938621f033364ec466ababd0f85b0
LAKSHYA SINGLA <lakshyasingla125@gmail.com>
2026-01-09 18:33:21 +0530

    Industry section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from './Components/Navbar';
 import Claudai from './Components/Claudai';
 import Theworld from './Components/Theworld'; 
+import Industry from './Components/Industry'; 
 import Trusted from './Components/Trusted'; 
 import CTA from './Components/CTA'; 
 import Footer from './Components/Footer';
@@ -10,6 +11,7 @@ export default function Home() {
     <Navbar />
     <Claudai />
     <Theworld/>
+    <Industry/>
     <Trusted/>
     <CTA/>
      <Footer/>
